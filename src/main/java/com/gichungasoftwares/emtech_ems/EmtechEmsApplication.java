7b921295f94e757c52a50c0ade6ff3a5aff79ad6
gichunga <gichungasteve6@gmail.com>
2024-11-13 09:44:28 +0300

    initial project configuration

---
+++ src/main/java/com/gichungasoftwares/emtech_ems/EmtechEmsApplication.java
@@ -0,0 +1,13 @@
+package com.gichungasoftwares.emtech_ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtechEmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmtechEmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gichungasoftwares/emtech_ems/EmtechEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gichungasoftwares.emtech_ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtechEmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
